a82c8def81523b7fe3d12524382acfac5ba8d5a0
Andriy Pastukh <andriy.pastukh77@gmail.com>
2023-09-20 10:20:09 +0300

    Add .gitignore

--- wdio.conf.js
+++ wdio.conf.js
@@ -124,8 +124,18 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
-
+    // reporters: ['spec'],
+    reporters: [
+        'spec',
+        [
+          'allure',
+          {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: true,
+          },
+        ],
+      ],
     
     //
     // Options to be passed to Mocha.
